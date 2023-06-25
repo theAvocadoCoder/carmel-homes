547fe375460ee7077551694640f4ecaaf9968c81
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2023-06-25 12:45:53 +0100

    feat: hero section

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,17 +1,21 @@
 //eslint-disable-next-line no-unused-vars
 import React from "react";
 
+import Hero from "./sections/Hero";
+import HowWeWork from "./sections/HowWeWork";
+import WhatIsRTO from "./sections/WhatIsRTO";
+
 function Homepage() {
   return (
     <div>
-      <div className="bg-zinc-400 h-[450px] flex justify-center items-center">
-        Home Dummy Section 1
+      <div className="">
+        <Hero />
       </div>
-      <div className="bg-blue-950 h-[450px] flex justify-center items-center text-white">
-        Home Dummy Section 2
+      <div className="">
+        <HowWeWork />
       </div>
-      <div className="bg-stone-400 h-[450px] flex justify-center items-center">
-        Home Dummy Section 3
+      <div className="">
+        <WhatIsRTO />
       </div>
       <div className="bg-slate-950 h-[450px] flex justify-center items-center text-white">
         Home Dummy Section 4
---
+++ src/pages/Homepage/sections/Hero.jsx
@@ -0,0 +1,32 @@
+import Button from "global/Button";
+import { Link } from "react-router-dom";
+// import whiteCouple from "homepage/white-couple.png";
+
+function Hero() {
+  return (
+    <div className="[background-blend-mode:multiply] bg-slate-500 bg-[url('homepage/hero.png')] bg-cover bg-center bg-no-repeat w-full lg:h-[600px] flex justify-evenly">
+      <div className="flex flex-col gap-6 md:max-w-[50%] lg:max-w-[37%] px-[clamp(1.25rem,7%,2.5rem)] py-28 md:pt-40 self-center">
+        <p className="text-white text-5xl [letter-spacing:0.15rem] font-extrabold" >
+          Get a Step Closer to <span className="text-brown-primary">Home</span> Ownership
+        </p>
+        <div className="flex gap-3">
+          <Link to="/apply-now">
+            <Button theme="primary" className="px-4 py-3 shadow-slate-600 shadow-sm">
+              Apply Now
+            </Button>
+          </Link>
+
+          <Link to="/">
+            <Button theme="secondary" className="px-4 py-3 shadow-slate-200 shadow-sm">
+              Request a Callback
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <div className="md:bg-[url('homepage/white-couple.png')] md:bg-cover md:bg-center md:bg-no-repeat md:w-[465px] md:h-[334px] lg:w-[623px] lg:h-[462px] self-end" role="img" aria-roledescription="image of a caucasian couple" />
+      {/* <img src={whiteCouple} alt="a white hetero couple" className="w-[623px] self-end" /> */}
+    </div>
+  )
+}
+
+export default Hero
