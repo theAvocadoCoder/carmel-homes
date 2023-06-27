e9f10fa47fa56e6ce88aceea5862eeff2cc51ffc
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2023-06-27 14:49:24 +0100

    fix: implement scroll to hash for rto section

--- src/pages/Homepage/sections/WhatIsRTO.jsx
+++ src/pages/Homepage/sections/WhatIsRTO.jsx
@@ -1,7 +1,12 @@
-import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 import Button from "global/Button";
 
 function WhatIsRTO() {
+  const scrollWithOffset = (el) => {
+    const yCoordinate = el.getBoundingClientRect().top + window.scrollY;
+    const yOffset = -80;
+    window.scrollTo({ top: yCoordinate + yOffset, behavior: "smooth" });
+  }
   return (
     <div className="flex flex-col lg:flex-row items-center justify-center gap-12 lg:gap-32 2xl:gap-44 px-8 lg:px-24 py-12 lg:py-32 [background-image:linear-gradient(to_bottom,black,transparent),url('homepage/what-is-rto.png')] bg-cover bg-center bg-no-repeat">
       <div className="flex flex-col text-white text-[1rem] gap-8 lg:max-w-[40%]">
@@ -12,11 +17,11 @@ function WhatIsRTO() {
         <p>
           Attain your goal of home ownership. We work with you to repair what has been holding you back from qualifying for a traditional mortgage. This is done while you are living in your future home.
         </p>
-        <Link to="/what-is-rent-to-own/#how-it-works">
+        <HashLink to="/what-is-rent-to-own#how-it-works" scroll={el => scrollWithOffset(el)}>
           <Button theme="secondary" className="px-4 py-3 text-[1.25rem] font-semibold">
             Learn How It Works
           </Button>
-        </Link>
+        </HashLink>
       </div>
       <div className="w-full h-44 lg:w-[50rem] lg:h-[18rem] flex items-center justify-center bg-zinc-400 text-white">
         Video Placeholder
