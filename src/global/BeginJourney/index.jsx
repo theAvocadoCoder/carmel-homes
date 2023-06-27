3a5c427a66bec67c51adb1fcef03f5a3bb44966a
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2023-06-27 16:25:46 +0100

    style: cosmetic fixes

--- src/global/BeginJourney/index.jsx
+++ src/global/BeginJourney/index.jsx
@@ -6,8 +6,8 @@ import beginJourney2Img from "assets/begin-journey-2.webp";
 function BeginJourney() {
   return (
     <div className="flex flex-col px-8 py-20 items-center justify-center">
-      <div className="flex flex-col items-center md:flex-row md:justify-center md:bg-brown-accent/40 gap-20 lg:gap-0 rounded-[3rem] md:py-16 md:px-8 lg:py-20 lg:px-12 max-w-[80rem]">
-        <div className="flex flex-col gap-16 lg:shrink max-w-[50%]">
+      <div className="flex flex-col items-start md:items-center md:flex-row md:justify-center md:bg-brown-accent/40 gap-20 lg:gap-0 rounded-[3rem] md:py-16 md:px-8 lg:py-20 lg:px-12 max-w-[80rem]">
+        <div className="flex flex-col gap-16 lg:shrink max-w-[60%]">
           <p className="text-[2.5rem] [line-height:1] lg:text-5xl font-extrabold max-w-[28rem]">Begin Your <span className="text-brown-primary">Homeowner</span> Journey With Us</p>
           <Link to="/apply-now" className="w-fit">
             <Button theme="primary" className="px-4 py-3 text-[1.5rem] font-bold">
