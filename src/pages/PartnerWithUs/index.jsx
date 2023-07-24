dc09e9a4055e8fdc22b65f39b172658843f8c48f
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2023-07-24 12:29:14 +0100

    feat: implement partner with us page layout

--- src/pages/PartnerWithUs/index.jsx
+++ src/pages/PartnerWithUs/index.jsx
@@ -1,25 +1,16 @@
 //eslint-disable-next-line no-unused-vars
 import React from "react";
+import Hero from "./sections/Hero";
+import WhyPartner from "./sections/WhyPartner";
+import PartnerWithCarmel from "./sections/PartnerWithCarmel";
 
 function PartnerWithUs() {
   return (
-    <div>
-      <div className="bg-zinc-400 h-[450px] flex justify-center items-center">
-        Partner with Us Dummy Section 1
-      </div>
-      <div className="bg-blue-950 h-[450px] flex justify-center items-center text-white">
-        Partner with Us Dummy Section 2
-      </div>
-      <div className="bg-stone-400 h-[450px] flex justify-center items-center">
-        Partner with Us Dummy Section 3
-      </div>
-      <div className="bg-slate-950 h-[450px] flex justify-center items-center text-white">
-        Partner with Us Dummy Section 4
-      </div>
-      <div className="bg-white h-[450px] flex justify-center items-center">
-        Partner with Us Dummy Section 5
-      </div>
-    </div>
+    <>
+      <Hero />
+      <WhyPartner />
+      <PartnerWithCarmel />
+    </>
   )
 }
 
