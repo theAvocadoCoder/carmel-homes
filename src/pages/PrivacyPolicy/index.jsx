acf7e49ed63336626f5d6abfdd7d84413cad30e5
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2023-07-27 23:56:30 +0100

    feat: implement privacy policy page layout

--- src/pages/PrivacyPolicy/index.jsx
+++ src/pages/PrivacyPolicy/index.jsx
@@ -1,25 +1,37 @@
 //eslint-disable-next-line no-unused-vars
-import React from "react";
+import policyText from "./policy";
+//eslint-disable-next-line no-unused-vars
+import render from "./policyRenderers";
+import useStore from "src/store";
 
 function PrivacyPolicy() {
+  const [
+    copiedToClipBoard, showPopUp, hidePopUp] = useStore(
+      (state) => [state.copiedToClipBoard, state.showPopUp, state.hidePopUp]
+    )
   return (
-    <div>
-      <div className="bg-zinc-400 h-[450px] flex justify-center items-center">
-        Privacy Policy Dummy Section 1
-      </div>
-      <div className="bg-blue-950 h-[450px] flex justify-center items-center text-white">
-        Privacy Policy Dummy Section 2
-      </div>
-      <div className="bg-stone-400 h-[450px] flex justify-center items-center">
-        Privacy Policy Dummy Section 3
-      </div>
-      <div className="bg-slate-950 h-[450px] flex justify-center items-center text-white">
-        Privacy Policy Dummy Section 4
-      </div>
-      <div className="bg-white h-[450px] flex justify-center items-center">
-        Privacy Policy Dummy Section 5
-      </div>
-    </div>
+    <>
+      <section className="p-8 md:p-16 lg:p-36 py-4 md:py-8 lg:py-16">
+        <h1 className="text-[2.25rem] md:text-[2.75rem] lg:text-[3.25rem] xl:text-[3.75rem] font-extrabold">Privacy Policy</h1>
+        <span>Last updated: January 02, 2023</span>
+
+        <p>
+          This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.
+        </p>
+        <p>
+          We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. 
+        </p>
+      </section>
+
+      <section className="relative p-8 md:p-16 lg:p-36 2xl:max-w-[75%] pt-4 md:pt-8 lg:pt-16">
+        {
+          policyText.map((element, index) => (
+            render(element, index, copiedToClipBoard, showPopUp, hidePopUp)
+          ))
+        }
+      </section>
+      
+    </>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,7 @@ export default defineConfig({
     alias: {
       ...filePaths,
       ...pageImgPaths,
+      "src": "/src/"
     }
   }
 })
