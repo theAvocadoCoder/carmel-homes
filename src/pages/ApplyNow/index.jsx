dad715bf022c85a93ef91325db105bb1d0dfdf12
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2023-08-18 20:15:43 +0100

    feat: integrate the forms from follow-up guys

--- src/pages/ApplyNow/index.jsx
+++ src/pages/ApplyNow/index.jsx
@@ -1,11 +1,12 @@
 import Benefits from "./sections/Benefits";
-import FormSteps from "./sections/FormSteps";
+// import FormSteps from "./sections/FormSteps";
+import FUGFormSteps from "./sections/FUGFormSteps";
 
 function ApplyNow() {
   return (
-    <div className="flex flex-col lg:flex-row lg:justify-center p-5 md:p-10 2xl:py-56 bg-[url('applynow/hero.webp')] bg-center bg-cover bg-no-repeat">
+    <div className="flex flex-col lg:flex-row lg:justify-center p-0 sm:p-5 md:p-10 2xl:py-56 bg-[url('applynow/hero.webp')] bg-center bg-cover bg-no-repeat">
       <Benefits />
-      <FormSteps />
+      <FUGFormSteps />
     </div>
   )
 }
---
+++ src/pages/ApplyNow/sections/FUGFormSteps.jsx
@@ -0,0 +1,26 @@
+
+function FUGFormSteps() {
+  return (
+    <div className="w-full md:w-[clamp(30rem,99%,50rem)] h-fit">
+      <iframe
+        src="https://api.leadconnectorhq.com/widget/form/cqSLB9b636cBvGYDKVIP"
+        style={{ width: "100%", height: "100%" }}
+        id="inline-cqSLB9b636cBvGYDKVIP" 
+        data-layout="{'id':'INLINE'}"
+        data-trigger-type="alwaysShow"
+        data-trigger-value=""
+        data-activation-type="alwaysActivated"
+        data-activation-value=""
+        data-deactivation-type="neverDeactivate"
+        data-deactivation-value=""
+        data-form-name="Apply Now"
+        data-height="1820"
+        data-layout-iframe-id="inline-cqSLB9b636cBvGYDKVIP"
+        data-form-id="cqSLB9b636cBvGYDKVIP"
+        title="Apply Now"
+      ></iframe>
+    </div>
+  )
+}
+
+export default FUGFormSteps
