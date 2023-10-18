c782770e8a03c68a4b5efe1d5a4a8cfab163038d
Kelechi Nwa-uwa <nwauwa.kelechi.e@gmail.com>
2023-10-18 12:40:18 +0100

    feat: add Google ad tag to rent to own page

--- src/pages/WhatIsRentToOwn/index.jsx
+++ src/pages/WhatIsRentToOwn/index.jsx
@@ -11,6 +11,15 @@ function WhatIsRentToOwn() {
       <Helmet>
         <title>Carmel Homes Ltd - What is Rent to Own?</title>
         <meta name="description" content="Rent-to-own is a home ownership program designed to make your dream of home ownership a reality. To get more details on how it works, visit this page." />
+        {/*Google tag (gtag.js)*/}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YT7M87W0RE"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-YT7M87W0RE');
+        </script>
       </Helmet>
       <h1 className="h-0 opacity-0">What is Rent to Own?</h1>
       <WhoCanUseRTO />
