374b2aa67c9556370426ace4eee8d848eee2b4e3
Kelechi Nwa-uwa <nwauwa.kelechi.e@gmail.com>
2023-10-18 12:35:54 +0100

    feat: add Google ad tag to about us page

--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -11,6 +11,15 @@ function About() {
       <Helmet>
         <title>Carmel Homes Ltd - About Us</title>
         <meta name="description" content="Our mission is to make home ownership a reality again. Buying a home shouldn’t be hard. Rent-to-own can make home ownership in Canada a reality for you." />
+        {/*Google tag (gtag.js)*/}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YT7M87W0RE"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-YT7M87W0RE');
+        </script>
       </Helmet>
       <h1 className="h-0 opacity-0">About Us</h1>
       <Hero />
