648c558509e7c371c871f1444b3b9c393f987293
Kelechi Nwa-uwa <nwauwa.kelechi.e@gmail.com>
2023-10-18 12:38:45 +0100

    feat: add Google add tag to partner with us page

--- src/pages/PartnerWithUs/index.jsx
+++ src/pages/PartnerWithUs/index.jsx
@@ -10,6 +10,15 @@ function PartnerWithUs() {
       <Helmet>
         <title>Carmel Homes Ltd - Partner with Us</title>
         <meta name="description" content="Expand your addressable market instantly! Partnering with us can help convert reluctant renters into homeowners, increasing the chances of converting a sale." />
+        {/*Google tag (gtag.js)*/}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YT7M87W0RE"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-YT7M87W0RE');
+        </script>
       </Helmet>
       <h1 className="h-0 opacity-0">Partner with Us</h1>
       <Hero />
