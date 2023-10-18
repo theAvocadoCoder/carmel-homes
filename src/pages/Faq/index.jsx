bf43af42b7d0b1fe0b8549d7d67514bfdc470da9
Kelechi Nwa-uwa <nwauwa.kelechi.e@gmail.com>
2023-10-18 12:37:58 +0100

    feat: add Google ad tag to faq page

--- src/pages/Faq/index.jsx
+++ src/pages/Faq/index.jsx
@@ -9,6 +9,15 @@ function Faq() {
       <Helmet>
         <title>Carmel Homes Ltd - FAQs</title>
         <meta name="description" content="Find some commonly asked questions to give you more information about the Rent to Own Home Ownership Program." />
+        {/*Google tag (gtag.js)*/}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YT7M87W0RE"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-YT7M87W0RE');
+        </script>
       </Helmet>
       <h1 className="h-0 opacity-0">FAQs</h1>
       <Hero />
